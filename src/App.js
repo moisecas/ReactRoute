ae43543bc4a24539f9fa8457d6ab5913b5268cf8
Your Name <moisesjuniorcas20@gmail.com>
2022-04-01 15:28:21 -0500

    rutas con parametros

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
 import Contacto from "./components/Contacto";
 import Inicio from "./components/Inicio";
 import Nosotros from "./components/Nosotros";
+import User from "./components/User";
 
 function App() {
   return ( //router para navegación dinamica, por ejemplo al ir a una sección del sitio no existente me siga mostrando contenido
@@ -23,6 +24,7 @@ function App() {
         <hr />
 
         <Routes>
+          <Route path="/nosotros/:id" element={<User />} />                                             
           <Route path="/" element={<Inicio />} />  
           <Route path="/nosotros" element={<Nosotros />} />  
           <Route path="/contacto" element={<Contacto />} />  
--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom' //usar el link dentro de este componente 
 
 const Nosotros = () => {
 
@@ -12,10 +13,10 @@ const Nosotros = () => {
     }, []) //para que el useeffect se ejecute una sola vez 
 
     const obtenerDatos = async () => { //funcion de llamado a la api 
-        const data = await fetch('https://jsonplaceholder.typicode.com/users') //llamado al url, retorna un json
+        const data = await fetch('https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations') //llamado al url, retorna un json
         const users = await data.json() //la  transformamos a json 
         //console.log(users)
-        setEquipo(users) //los vamos a visualizar en el ul, mira dentro del return
+        setEquipo(users.civilizations) //los vamos a visualizar en el ul, mira dentro del return
     }
 
   return (
@@ -23,8 +24,12 @@ const Nosotros = () => {
         <h1>Nosotros</h1>
         <ul>
             {
-                equipo.map(item => (
-                    <li key={item.id}>{item.name} - {item.email} </li> // el li necesita un key que lo sacamos de la api así como los datos que queremos mostrar {}
+                equipo.map(item => ( //creando urls dinámicas con el link para traer los datos de la api
+                    <li key={item.id}>
+                    <Link to={`/nosotros/ ${item.id}`}> 
+                        {item.name} - {item.expansion} 
+                    </Link>
+                    </li> // el li necesita un key que lo sacamos de la api así como los datos que queremos mostrar {}
 
                 ))
             }
